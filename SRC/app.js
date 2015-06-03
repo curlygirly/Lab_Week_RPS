49595298b9c98a864f316994ae0d13d0a4e4ee2d
Jennifer Arad-Marks <jlaradmarks@gmail.com>
2015-06-03 17:15:14 +0100

    HTML page now displays choice and results

--- SRC/app.js
+++ SRC/app.js
@@ -4,19 +4,30 @@ window.onload=function(){
   tellUs();
 };
 
+tellUs = function() {
+  document.getElementById('whatsGoingOn').innerHTML = play.playerChoice;
+};
+
+decision = function () {
+  document.getElementById('whatsGoingOn').innerHTML = play.message;
+};
+
 playerChoosesRock = function(){
   play.chooseRock();
   console.log(play.playerChoice);
+  tellUs();
 };
 
 playerChoosesPaper = function(){
   play.choosePaper();
   console.log(play.playerChoice);
+  tellUs();
 };
 
 playerChoosesScissors = function(){
   play.chooseScissors();
   console.log(play.playerChoice);
+  tellUs();
 };
 
 winnerIsDecided = function(){
@@ -25,8 +36,6 @@ winnerIsDecided = function(){
   console.log(play.computerChoice);
   play.versus();
   console.log(play.message);
+  decision();
 };
 
-tellUs = function() {
-  document.getElementById('whatsGoingOn').innerHTML = play.playerChoice;
-};
